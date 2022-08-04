a99a74eaa94dd991766213a81a823a987c143b3f
Dmitriy <simpa-baki@mail.ru>
2022-08-05 00:16:36 +0300

    example007

---
+++ Examples007_Dog/Program.cs
@@ -0,0 +1,29 @@
+﻿int count = 0;
+int D = 10000; //расстояние между друзьями
+int S1 = 1; // скорость первого друга
+int S2 = 2; // скорость второго друга
+int Sd = 5; //скорость собаки
+int F = 2;
+int T = 0;
+
+while (D > 10)
+{
+    if (F == 1)
+    {
+        T = D / (S1 + Sd);
+        F = 2;
+        D = D - (S1 + S2)*T;
+        count++;
+    }
+    else
+    {
+        T = D / (S2 + Sd);
+        F = 1;
+        D = D - (S1 + S2)*T;
+        count++;
+    }
+
+}
+Console.Write("Собака пробежала ");
+Console.Write(count);
+Console.Write(" раз ");
